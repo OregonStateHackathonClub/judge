84b13030a4450d1a8771a08f345bfd9f56179594
pilijara <blancojp@oregonstate.edu>
2025-07-22 21:41:38 -0700

    Made submissions page with react form hooks that steps through

--- src/app/[year]/submission/page.tsx
+++ src/app/[year]/submission/page.tsx
@@ -1,37 +1,244 @@
-import { Suspense } from "react";
-import { Title } from "./title"
-import { Description } from "./description"
+"use client";
 
-const fetchUserWithDelay = () =>
-  new Promise((res) =>
-    setTimeout(
-      async () => res(await (await fetch("https://randomuser.me/api/")).json()),
-      2000
-    )
-  );
+import React  from "react"
+import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
 
-export default function Page({ params }: { params: { year: string } }) {
-	const year = params.year;
-  const write = async () => {
-    "use server";
+const MAX_STEPS = 4
 
-    console.log("test");
-  };
+export default function Page ({ params }: { params: { year: string } }) {
+  const [ formStep, setFormStep ] = React.useState(0)
+  const { 
+    watch, 
+    register,
+    handleSubmit,
+    formState: { errors, isValid } 
+  } = useForm({ mode: "all" })
+
+  // function to move onto the next step of th eform
+  const nextStepForm = () => {
+    setFormStep(cur => cur + 1)
+  }
+
+  // function to move to the step before of the form
+  const backStepForm = () => {
+    setFormStep(cur => cur - 1)
+  }
+
+  // function to render buttons for next and back step of form, alongside a finsh button for the end
+  // Finish button currently does nothing()->
+  //      Should direct user back to lannding page
+
+  // My thoughts on the database:
+  // to be able to auto save, after every step the information should be saved to the db
+  // though if the last step is not reached, the post should no be visisble to the public
+  // a new param might be needed to be added to the submissions to verify taht its complete
+  // if then complete it will be displayed in the landing page and everwhere else, this would come
+  // through the user making it all the way to the end of the submission process, else, the currently
+  // provided info shoul be saved but not authorised to be published
+  // then the edit submission should use this same form just that the info is already filled out
+  // if the user makes changes it should only be showcased publicly if user makes it to the end,
+  // the new data could prbably be stored locally and the updated once the user finishes as f it was a new submission, altering their old one
+
+  const renderButton = () => {
+    if(formStep === 3) {
+      // This is the finish button and the onclick command should probably direct the user back to the landing page
+      return (  
+        <Button
+          type="button"
+          onClick={() => {setFormStep(0)}}
+        >
+            Finish
+        </Button>
+      )
+    } else if( formStep === 2 ){
+      return (
+        <>
+          {/* Back step button */}
+          <Button
+            type="button"
+            onClick={backStepForm}
+          >
+            back
+          </Button>
+          <Button
+            type="button"
+            onClick={handleSubmit(submitForm)}
+          >
+            Submit Project
+          </Button>
+        </>
+      )
+    } else if ( formStep > 0) {
+      return (
+        <>
+          {/* Back step button */}
+          <Button
+            type="button"
+            onClick={backStepForm}
+          >
+            back
+          </Button>
+          {/* Next Step button */}
+          <Button
+            disabled={!isValid}
+            type="button"
+            onClick={nextStepForm}
+          >
+            Next
+          </Button>
+        </>
+      )
+    } else {
+      return (
+        <>
+          {/* Next Step button */}
+          <Button
+            disabled={!isValid}
+            type="button"
+            onClick={nextStepForm}
+          >
+            Next
+          </Button>
+        </>
+      )
+    }
+  }
+  // submission
+  const submitForm = (values : any): void => {
+    window.alert(JSON.stringify(values, null, 2))
+    nextStepForm();
+  }
 
   return (
     <>
-      <Title />
-      <Description />
-      <button onClick={write}>test server action</button>
-      <Suspense fallback={"Loading..."}>
-        <PeopleData />
-      </Suspense>
-			{year}
-    </>
-  );
-}
+    <form onSubmit={handleSubmit(submitForm)}>
+      <div><p>Step {formStep + 1} of {MAX_STEPS}</p></div>
+      {/* Title and Description of project */}
+      {formStep === 0 && (
+        <section>
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            <label htmlFor="projectTitle">Project Title:</label>
+            <input
+              type="text"
+              id="projectTitle"
+              className="border border-black px-2 py-1 rounded w-[665px] ml-[50px]"
+              {...register("projectTitle",{
+                required: "Please enter a Title for your project",
+                maxLength: {
+                  value: 100,
+                  message: "Title must be under 100 characters"
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9 ]+$/,
+                  message: "Only letters and numbers allowed"
+                }
+              })}
+            />
+          </div>
+          {errors.projectTitle && (
+            <p className="text-red-500 text-sm">{String(errors.projectTitle.message)}</p>
+          )}
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            <label htmlFor="projectDescription">Project Description:</label>
+            <textarea
+              id="projectDescription"
+              className="border border-black px-2 py-1 rounded w-[665px] h-[222px]"
+              {...register("projectDescription", {
+                required: "Please enter a Description for your project",
+                maxLength: {
+                  value: 5000,
+                  message: "Description must be under 5000 characters"
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9 ]+$/,
+                  message: "Only letters and numbers allowed"
+                }
+              })}
+            />
+          </div>
+          {errors.projectDescription && (
+            <p className="text-red-500 text-sm">{String(errors.projectDescription.message)}</p>
+          )}
+        </section>
+      )}
 
-async function PeopleData() {
-  const data = await fetchUserWithDelay();
-  return <div>{JSON.stringify(data)}</div>;
+      {/* Project media */}
+      {formStep === 1 && (
+        <section>
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            <label htmlFor="githubLink">GitHub Link:</label>
+            <input
+              type="text"
+              id="githubLink"
+              className="border border-black px-2 py-1 rounded w-[665px] ml-[50px]"
+              {...register("githubLink",{
+                required: "Please enter a link to the Github repo"
+              })}
+            />
+          </div>
+          {errors.githubLink && (
+            <p className="text-red-500 text-sm">{String(errors.githubLink.message)}</p>
+          )}
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            <label htmlFor="youtubeLink">Youtube Link:</label>
+            <input
+              type="text"
+              id="youtubeLink"
+              className="border border-black px-2 py-1 rounded w-[665px] ml-[50px]"
+              {...register("youtubeLink",{
+                required: "Please enter a link to the YouTube video demo"
+              })}
+            />
+          </div>
+          {errors.youtubeLink && (
+            <p className="text-red-500 text-sm">{String(errors.youtubeLink.message)}</p>
+          )}
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            <label htmlFor="uploadPhotos">Upload Photos:</label>
+            <input
+              type="text"
+              id="uploadPhotos"
+              className="border border-black px-2 py-1 rounded w-[665px] ml-[50px]"
+              {...register("uploadPhotos",{
+                required: "Please upload images of the project"
+              })}
+            />
+          </div>
+          {errors.uploadPhotos && (
+            <p className="text-red-500 text-sm">{String(errors.uploadPhotos.message)}</p>
+          )}
+      </section>
+      )}
+
+      {/* Review of submissions */}
+      {formStep === 2 && (
+        <section>
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            Review Submission
+          </div>
+          <div className="ml-4">
+            <p><strong>Title:</strong> {watch("projectTitle")}</p>
+            <p><strong>Description:</strong> {watch("projectDescription")}</p>
+            <p><strong>GitHub:</strong> {watch("githubLink")}</p>
+            <p><strong>YouTube:</strong> {watch("youtubeLink")}</p>
+            <p><strong>Photos:</strong> {watch("uploadPhotos")}</p>
+          </div>
+      </section>
+      )}
+      {/* Final Step, let user know they Finished */}
+      {formStep === 3 && (
+        <section>
+          <div className="flex gap-4 mb-4 mt-4 ml-4">
+            Congratulations your submission has been saved!
+          </div>
+      </section>
+      )}
+      {renderButton()}
+      <pre>
+        {JSON.stringify(watch(), null, 2)}
+      </pre>
+    </form>
+    </>
+  )
 }
--- src/app/[year]/submission/title.tsx
+++ src/app/[year]/submission/title.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export function Title() {
     return (
-        <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem", marginLeft: "1rem"}}>
+        <form style={{ display: "flex", gap: "1rem", marginBottom: "1rem", marginTop: "1rem", marginLeft: "1rem"}}>
             <label htmlFor="project-title">Project Title:</label>
             <input 
                 type="text" 
@@ -10,6 +10,6 @@ export function Title() {
                 id="project-title"
                 style={{ border: "1px solid #000", padding: "0.25rem 0.5rem", borderRadius: "4px", width: "665px", marginLeft: "50px"}}
                 />
-        </div>
+        </form>
 )
 }
