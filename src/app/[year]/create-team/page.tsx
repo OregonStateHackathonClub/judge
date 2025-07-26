3148f2ac5094a898f350aeeeef2ba8af3371ea18
Adam <abobich675@gmail.com>
2025-07-26 15:18:35 -0700

    shadcn forms for create-team

--- src/app/[year]/create-team/page.tsx
+++ src/app/[year]/create-team/page.tsx
@@ -1,53 +1,136 @@
+'use client'
+
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Label } from "@/components/ui/label"
-import { Form, FormField } from "@/components/ui/form"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { set, z } from "zod"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { useForm } from "react-hook-form"
 
 
+
+const formSchema = z.object({
+  teamName: z.string().min(4, {message: "Username must be at least 4 characters.",}),
+  lft: z.boolean().optional(),
+  email: z.string().optional(),
+  description: z.string().optional(),
+})
 
 export default function Home() {
+  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      teamName: "",
+    },
+  })
+  
+  // TODO: submit
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  function updateLFT(lft: boolean) {
+    let components = document.getElementsByClassName("lft-component")
+    for (let i = 0; i < components.length; i++) {
+      if (lft) {
+        components[i].classList.remove("hidden");
+      }
+      else {
+        components[i].classList.add("hidden");
+      }
+    }
+    console.log(lft)
+  }
+
   return (
     <div className="w-[60%] mx-auto">
-      <div className="text-4xl pt-5">
+      <div className="text-4xl pt-5 pb-10">
         Team Creation
       </div>
 
-      <form className="pl-10">
-        <div className="text-2xl pt-5">
-          Team Name
-        </div>
-        <Input className="w-50 m-2.5"/>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="teamName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Team Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Amazing Team Name" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your team's public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="text-2xl pt-5">
-          Team Members
-        </div>
-        <ul className="space-y-2 pl-10">
-          <li>Your Username</li>
-          <li><Button variant="outline">Add Teammate</Button></li>
-        </ul>
+          <FormField
+            control={form.control}
+            name="lft"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center gap-2 mb-5">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={(checked) => {
+                      field.onChange(checked);
+                      updateLFT(Boolean(checked));
+                    }}
+                  />
+                </FormControl>
+                <FormLabel>
+                  Looking for additional team members
+                </FormLabel>
+              </FormItem>
+            )}
+          />
 
-        <div className="flex text-xl gap-2 pt-10">
-          <Checkbox id="lft-box"/>
-          <Label htmlFor="lft-box">Looking for additional team members</Label>
-        </div>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="pl-10 lft-component hidden">
+                <FormLabel>Contact Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="benny@beaverhacks.com" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-        <div className="p-5 pl-10">
-          <div>
-            Contact Email
-          </div>
-          <Input type="email" placeholder="benny@beaverhacks.com" className="m-2.5 w-75" />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="pl-10 lft-component hidden">
+                <FormLabel>Brief Project Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Prospective teammates should know..." {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-          <div> 
-            Brief Project Description
-          </div>
-          <Textarea className="m-2.5"/>
-        </div>
-
-        <Button className="bg-green-500 hover:bg-green-600 w-30 h-10 rounded-4xl">Accept Team</Button>
-      </form>
-
+          <Button type="submit" className="bg-green-500 hover:bg-green-600 rounded-4xl">Accept Team</Button>
+        </form>
+      </Form>
     </div>
   );
 }
