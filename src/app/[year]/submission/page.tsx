0650132262544d5f302b2d93199944e8b0431406
pilijara <blancojp@oregonstate.edu>
2025-08-13 15:30:29 -0700

    Fixed format and worked on the zod schema

--- src/app/[year]/submission/page.tsx
+++ src/app/[year]/submission/page.tsx
@@ -13,6 +13,7 @@ import { Textarea } from "@/components/ui/textarea"
 import { Progress } from '@/components/ui/progress'
 import { motion, AnimatePresence } from 'motion/react'
 import { useMultiStepForm } from './useForm'
+import { JSX } from "react/jsx-runtime"
 
 export function DraftForm() {
   const form = useForm < z.infer < typeof formSchema >> ({
@@ -47,168 +48,171 @@ export function DraftForm() {
 /**
  * Used to render a multi-step form in preview mode
  */
-export function MultiStepViewer({
-  form,
-}: {
-  form: any;
-}) {
+export function MultiStepViewer({ form }: { form: any; }) {
   const stepFormElements: {
     [key: number]: JSX.Element
   } = {
-    1: <div><h1 className="text-3xl font-bold">Project Title & Description</h1>
-<div className="py-3 w-full">
-                <Separator />
-              </div>
-
-          <div className="flex items-center justify-between flex-wrap sm:flex-nowrap w-full gap-2">
-            <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>Title</FormLabel> *
-                      <FormControl>
-                        <Input
-                          placeholder="Enter your project's title"
-                          type={"text"}
-                          value={field.value}
-                          onChange={(e) => {
-                            const val = e.target.value;
-                            field.onChange(val);
-                          }}
-                        />
-                      </FormControl>
-                      
-                      <FormMessage />
-                  </FormItem>
-                  )
-                }
-              /><FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>Description</FormLabel> *
-                      <FormControl>
-                        <Input
-                          placeholder="Enter a short description"
-                          type={"url"}
-                          value={field.value}
-                          onChange={(e) => {
-                            const val = e.target.value;
-                            field.onChange(val);
-                          }}
-                        />
-                      </FormControl>
-                      
-                      <FormMessage />
-                  </FormItem>
-                  )
-                }
-              />
-          </div>
-<div className="py-3 w-full">
-                <Separator />
-              </div></div>,
-    2: <div><h1 className="text-3xl font-bold">Project Info</h1>
-<div className="py-3 w-full">
-                <Separator />
-              </div>
-
-        <FormField
-          control={form.control}
-          name="mainDescription"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Main Description</FormLabel> *
+    // Step 1
+    1: <div> <h1 className="text-3xl font-bold">Project Title & Mini-Description</h1>
+      <div className="py-3 w-full">
+        <Separator />
+      </div>
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>Title*</FormLabel>
               <FormControl>
-                <Textarea
-                  {...field}
-                  placeholder="Enter your project description"
-                  className="resize-none"
+                <Input
+                  placeholder="Enter your project's title"
+                  type={"text"}
+                  value={field.value}
+                  onChange={(e) => {
+                    const val = e.target.value;
+                    field.onChange(val);
+                  }}
                 />
               </FormControl>
-              
               <FormMessage />
             </FormItem>
-          )}
-        />
-<FormField
-                control={form.control}
-                name="github"
-                render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>GitHub</FormLabel> *
-                      <FormControl>
-                        <Input
-                          placeholder="Enter your GitHub link..."
-                          type={"url"}
-                          value={field.value}
-                          onChange={(e) => {
-                            const val = e.target.value;
-                            field.onChange(val);
-                          }}
-                        />
-                      </FormControl>
-                      
-                      <FormMessage />
-                  </FormItem>
-                  )
-                }
+          )
+        }
+      />
+      <div className="m-4"/><FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormLabel>Description*</FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Enter a short description"
+                value={field.value}
+                onChange={(e) => {
+                  const val = e.target.value;
+                  field.onChange(val);
+                }}
               />
-<FormField
-                control={form.control}
-                name="youtube"
-                render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>YouTube</FormLabel> *
-                      <FormControl>
-                        <Input
-                          placeholder="Provide your video URL"
-                          type={"url"}
-                          value={field.value}
-                          onChange={(e) => {
-                            const val = e.target.value;
-                            field.onChange(val);
-                          }}
-                        />
-                      </FormControl>
-                      
-                      <FormMessage />
-                  </FormItem>
-                  )
-                }
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          )
+        }
+      />  
+      <div className="py-3 w-full">
+        <Separator />
+      </div>
+    </div>,
+    // Step 2
+    2: <div> <h1 className="text-3xl font-bold">Main Description</h1>
+    <div className="py-3 w-full">
+        <Separator />
+      </div>
+      <FormField
+        control={form.control}
+        name="mainDescription"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Main Description*</FormLabel>
+            <FormControl>
+              <Textarea
+                {...field}
+                placeholder="Enter your project description"
+                className="resize-none"
               />
-<FormField
-                control={form.control}
-                name="photos"
-                render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>Photo Gallery</FormLabel> *
-                      <FormControl>
-                        <Input
-                          placeholder="Upload your photos"
-                          type={"url"}
-                          value={field.value}
-                          onChange={(e) => {
-                            const val = e.target.value;
-                            field.onChange(val);
-                          }}
-                        />
-                      </FormControl>
-                      
-                      <FormMessage />
-                  </FormItem>
-                  )
-                }
-              /></div>,
-    3: <div><h1 className="text-3xl font-bold">Project Review</h1>
-<p className="text-base">Please review the information below to ensure everything is correct:</p>
-<h3 className="text-xl font-bold">Title: </h3>
-<h3 className="text-xl font-bold">Main Description:</h3>
-<h3 className="text-xl font-bold">GitHub: </h3>
-<h3 className="text-xl font-bold">YouTube:</h3></div>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      /> 
+      <div className="py-3 w-full">
+        <Separator />
+      </div>
+    </div>,
+    // Step 3
+    3: <div><h1 className="text-3xl font-bold">Project Info</h1>
+      <div className="py-3 w-full">
+        <Separator />
+      </div>
+      <FormField
+        control={form.control}
+        name="github"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormLabel>GitHub*</FormLabel>
+            <FormControl>
+              <Input
+                placeholder="Enter your GitHub link..."
+                type={"url"}
+                value={field.value}
+                onChange={(e) => {
+                  const val = e.target.value;
+                  field.onChange(val);
+                }}
+              />
+            </FormControl>
+            <FormMessage />
+            </FormItem>
+          )
+        }
+      />
+      <div className="m-4"/><FormField
+        control={form.control}
+        name="youtube"
+        render={({ field }) => (
+          <FormItem className="w-full">
+          <FormLabel>YouTube</FormLabel>
+          <FormControl>
+            <Input
+              placeholder="Provide your video URL"
+              type={"url"}
+              value={field.value}
+              onChange={(e) => {
+                const val = e.target.value;
+                field.onChange(val);
+              }}
+            />
+          </FormControl>
+          <FormMessage />
+          </FormItem>
+          )
+        }
+      />
+      <div className="m-4"/><FormField
+        control={form.control}
+        name="photos"
+        render={({ field }) => (
+        <FormItem className="w-full">
+          <FormLabel>Photo Gallery*</FormLabel>
+          <FormControl>
+            <Input
+              placeholder="Upload your photos"
+              type={"url"}
+              value={field.value}
+              onChange={(e) => {
+                const val = e.target.value;
+                field.onChange(val);
+              }}
+            />
+          </FormControl>
+          <FormMessage />
+          </FormItem>
+          )
+        }
+      />
+      <div className="py-3 w-full">
+        <Separator />
+      </div>
+    </div>,
+    4: <div><h1 className="text-3xl font-bold">Project Review</h1>
+      <p className="text-base">Please review the information below to ensure everything is correct:</p>
+      <h3 className="text-xl font-bold">Title: </h3>
+      <h3 className="text-xl font-bold">Main Description:</h3>
+      <h3 className="text-xl font-bold">GitHub: </h3>
+      <h3 className="text-xl font-bold">YouTube:</h3></div>
   };
+
   const steps = Object.keys(stepFormElements).map(Number);
   const {
     currentStep,
@@ -231,59 +235,61 @@ export function MultiStepViewer({
     }
   } = form
   return (<div className="flex flex-col gap-2 pt-3">
-      <div className="flex flex-col items-center justify-start gap-1">
-        <span>
-          Step {currentStep} of {steps.length}
-        </span>
-        <Progress value={(currentStep / steps.length) * 100} />
-      </div>
-      <AnimatePresence mode="popLayout">
-        <motion.div
-          key={currentStep}
-          initial={{ opacity: 0, x: 15 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -15 }}
-          transition={{ duration: 0.4, type: 'spring' }}
-          className="flex flex-col gap-2"
-        >
-          {current}
-        </motion.div>
-      </AnimatePresence>
-      <div className="flex items-center justify-between gap-3 w-full pt-3">
-        <Button size="sm" variant="ghost" onClick={goToPrevious} type="button">
-          Previous
-        </Button>
-        {!isLastStep && (
-        <Button
-        size="sm"
-        type="button"
-        variant="secondary"
-        onClick={async () => {
-          // List the field names for the current step
-          const fieldsToValidate = currentStep === 1
-            ? ['name', 'description']
-            : currentStep === 2
-            ? ['mainDescription', 'github', 'youtube', 'photos']
-            : [];
-
-          const valid = await form.trigger(fieldsToValidate);
-          if (valid) {
-            goToNext();
-          }
-          // If not valid, errors will show automatically via <FormMessage />
-        }}
+    <div className="flex flex-col items-center justify-start gap-1">
+      <span>
+        Step {currentStep} of {steps.length}
+      </span>
+      <Progress value={(currentStep / steps.length) * 100} />
+    </div>
+    <AnimatePresence mode="popLayout">
+      <motion.div
+        key={currentStep}
+        initial={{ opacity: 0, x: 15 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -15 }}
+        transition={{ duration: 0.4, type: 'spring' }}
+        className="flex flex-col gap-2"
       >
-        Next
+        {current}
+      </motion.div>
+    </AnimatePresence>
+    <div className="flex items-center justify-between gap-3 w-full pt-3">
+      <Button size="sm" variant="ghost" onClick={goToPrevious} type="button">
+        Previous
       </Button>
-      )}
+      {!isLastStep && (
+      <Button
+      size="sm"
+      type="button"
+      variant="secondary"
+      onClick={async () => {
+        // List the field names for the current step
+        const fieldsToValidate = currentStep === 1
+          ? ['name', 'description']
+          : currentStep === 2
+          ? ['mainDescription']
+          : currentStep === 3
+          ? [ 'github', 'youtube', 'photos']
+          : [];
 
-      {isLastStep && (
-        <Button size="sm" type="submit">
-          {isSubmitting ? 'Submitting...' : 'Submit'}
-        </Button>
-      )}
-      </div>
-    </div>);
+        const valid = await form.trigger(fieldsToValidate);
+        if (valid) {
+          goToNext();
+        }
+        // If not valid, errors will show automatically via <FormMessage />
+      }}
+    >
+      Next
+    </Button>
+    )}
+
+    {isLastStep && (
+      <Button size="sm" type="submit">
+        {isSubmitting ? 'Submitting...' : 'Submit'}
+      </Button>
+    )}
+    </div>
+  </div>);
 }
 
 export default DraftForm
--- src/app/[year]/submission/schema.ts
+++ src/app/[year]/submission/schema.ts
@@ -9,10 +9,22 @@ export interface ActionResponse < T = any > {
   inputs ? : T
 }
 export const formSchema = z.object({
-  "name": z.string(),
-  "description": z.string(),
-  "mainDescription": z.string(),
-  "github": z.string(),
-  "youtube": z.string(),
-  "photos": z.string()
+  name: z.string().min(3).max(50,{
+    message: "Title should be between 3 and 50 characters"
+  }),
+  description: z.string().min(3).max(250,{
+    message: "Description should be between 3 and 250 characters"
+  }),
+  mainDescription: z.string().min(3).max(10000,{
+    message: "Description should be between 3 and 10000 characters"
+  }),
+  github: z.url({ 
+    hostname: /^github\.com$/,
+    message: "Must be a github link"
+  }),
+  youtube: z.url({ 
+    hostname: /^youtube\.com$/, 
+    message: "Must be a youtube link"
+  }).optional(),
+  photos: z.string()
 }); 
