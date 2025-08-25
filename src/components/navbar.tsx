84ce7c8b6f364a6b6f1b579e61a72356ff422d76
pilijara <blancojp@oregonstate.edu>
2025-08-25 16:56:48 -0700

    finished simple better-auth implementation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,15 @@
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
-
-//  /home/1234
-// /home/[id]
+import { Button } from './ui/button';
+import { signOut } from '@/lib/auth-client';
+import { authClient } from "@/lib/auth-client";
 
 export const Navbar = () => {
+  const {
+    data: session,
+  } = authClient.useSession();
+
   return (
     <div className="flex justify-between items-center px-8 h-20 bg-gray-100">
       <Link href='/' className='flex gap-5 items-center p-5 hover:bg-gray-200'>
@@ -13,9 +18,16 @@ export const Navbar = () => {
         </div>
         <h1>BeaverHacks Official Judging Platform</h1>
       </Link>
-      
-      <Link href='/Login' className='flex items-center px-5 hover:bg-gray-200'>Login</Link>
-      <Link href='/SignUp' className='flex items-center px-5 hover:bg-gray-200'>Sign Up</Link>
+      {session?.user ? (
+        <Button onClick={async () => {await signOut();}}>
+          Sign Out
+        </Button>
+      ) : (
+        <>
+          <Link href='/Login' className='flex items-center px-5 hover:bg-gray-200'>Login</Link>
+          <Link href='/SignUp' className='flex items-center px-5 hover:bg-gray-200'>Sign Up</Link>
+        </>
+      )}
     </div>
   )
 }
